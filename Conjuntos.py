18948ba2bb59a770c05534c06ccbddc018126e31
Carolina Martinez <carolinamtz091@gmail.com>
2023-05-27 20:55:54 -0600

    primera version

---
+++ Conjuntos.py
@@ -0,0 +1,20 @@
+#Conjuntos
+
+conjunto = set()
+
+conjunto = {1,2,3,"Carolina"}
+
+#Añadir un elemento
+conjunto.add(8)
+conjunto.add("Carolina")
+
+#Eliminar un elemenos
+conjunto.discard(3)
+
+#Buscar un elemento
+print(2 in conjunto)
+
+print(conjunto)
+
+
+#Carolina EM
